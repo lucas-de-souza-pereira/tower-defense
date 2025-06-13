32d27cfadf8d7b67ddf462bbf7549714744e0bd9
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-06-13 11:19:13 +0200

    add score +10 by enemy and timer

--- src/view/scoreview.cpp
+++ src/view/scoreview.cpp
@@ -9,12 +9,23 @@ void drawScore(sf::RenderWindow& window) {
     sf::Font font;
     font.loadFromFile("../assets/police/arial.ttf");
 
+    sf::Texture backgroundTexture;
+    backgroundTexture.loadFromFile("../assets/pictures/background_menu.PNG");
+    
+    sf::Sprite background(backgroundTexture);  
+
     auto scores = loadScores("../assets/data/scores.json");
+
+    sf::Text score("Score :", font, 40);
+    score.setPosition(450, 80);
+
+    window.draw(background);   
+    window.draw(score);   
 
     float y = 150.f;
     for (const auto& entry : scores) {
         sf::Text line(entry.name + ": " + std::to_string(entry.score), font, 30);
-        line.setPosition(300.f, y);
+        line.setPosition(450.f, y);
         line.setFillColor(sf::Color::White);
         window.draw(line);
         y += 50.f;
